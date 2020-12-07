c26195956af85e8dbf7428525b7600ebb03cc5b7
Connor Gannaway <ccgannaway@icloud.com>
2020-12-07 10:36:26 -0600

    database created

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Customer, Boat, Ticket, TicketItem
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Boat)
+admin.site.register(Ticket)
+admin.site.register(TicketItem)
--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Customer(models.Model):
+    firstName = models.CharField(max_length=25)
+    lastName = models.CharField(max_length=25)
+    email = models.EmailField()
+    phone = models.PositiveIntegerField(default=0)
+    dateAdded = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.firstName + ' ' + self.lastName
+
+class Boat(models.Model):
+    manufacturer = models.CharField(max_length=25)
+    model = models.CharField(max_length=25)
+    slip = models.CharField(max_length=5)
+    owner = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.slip
+
+class Ticket(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    boat = models.ForeignKey(Boat, on_delete=models.CASCADE)
+    timeCreated = models.DateTimeField(default=timezone.now)
+    timeDue = models.DateTimeField()
+    completed = models.BooleanField()
+
+    def __str__(self):
+        return self.customer + ' ' + self.timeDue
+
+class TicketItem(models.Model):
+    item = models.CharField(max_length=25)
+    description = models.TextField()
+    completed = models.BooleanField()
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.item + ' ' + self.ticket
